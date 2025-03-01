17bfd4688016999744cd9d0dcfe6cf23c3eba3db
luddenmath <luddenjacob2@gmail.com>
2025-03-01 10:54:11 -0600

    Update script.js

--- script.js
+++ script.js
@@ -3,8 +3,8 @@ let score = 0;
 
 // Function to generate random math questions
 const generateQuestion = () => {
-  const num1 = Math.floor(Math.random() * 10) + 1;
-  const num2 = Math.floor(Math.random() * 10) + 1;
+  const num1 = Math.floor(Math.random() * 10) + 10000;
+  const num2 = Math.floor(Math.random() * 10) + 10000;
   const operators = ['+', '-', '*', '/'];
   const operator = operators[Math.floor(Math.random() * operators.length)];
 
